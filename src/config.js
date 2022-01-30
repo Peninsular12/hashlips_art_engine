6d6af283e8ddf37abbe29f36d513f22e9f6e0a79
Peninsular12 <96875385+Peninsular12@users.noreply.github.com>
2022-01-31 00:09:53 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "Your Collection";
-const description = "Remember to replace this description";
+const namePrefix = "Peninsular nft";
+const description = "It's the legendary eye";
 const baseUri = "ipfs://NewUriToReplace";
 
 const solanaMetadata = {
